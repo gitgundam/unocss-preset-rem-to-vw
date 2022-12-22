0949133e64cd8baed7b533aacb9ae5e536a9bbba
王威 <wangwei@MacBook-Pro.local>
2022-12-22 13:47:24 +0800

    feat: add a preset

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import type { Preset } from "@unocss/core"
+
+const remRE = /(-?[\.\d]+)rem/g
+const pxRE = /(-?[\.\d]+)px/g
+
+export interface PxToVwOptions {
+	/**
+	 * 1rem = n px
+	 * @default 16
+	 *
+	 */
+	baseFontSize?: number
+	baseWidth?: number
+	unitPrecision?: number
+}
+
+export default function remToVwPreset(options: PxToVwOptions = {}): Preset {
+	const { baseFontSize = 16, baseWidth = 375, unitPrecision = 4 } = options
+
+	// 750px = 100vw  ; 1px = 100vw / 750px = 0.13vw
+	const pxToVw = (px: number) => ((100 / baseWidth) * px).toFixed(unitPrecision)
+
+	return {
+		name: "preset-rem-to-vw",
+		postprocess: (util) => {
+			util.entries.forEach((i) => {
+				const value = i[1]
+				if (value && typeof value === "string")
+					if (remRE.test(value))
+						i[1] = value.replace(
+							remRE,
+							(_, p1) => `${pxToVw(p1 * baseFontSize)}vw`
+						)
+					else if (pxRE.test(value))
+						i[1] = value.replace(pxRE, (_, p1) => `${pxToVw(p1)}vw`)
+			})
+		},
+	}
+}
